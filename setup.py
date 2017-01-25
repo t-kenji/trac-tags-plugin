1326857034761fee7684260e130d9bd6debc07a0
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2017-01-25 05:03:32 +0000

    0.10dev: Prepare for development of TracTags-0.10
    git-svn-id: https://trac-hacks.org/svn/tagsplugin/trunk@16201 7322e99d-02ea-0310-aa39-e9a107903beb

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
 
 setup(
     name='TracTags',
-    version='0.9',
+    version='0.10',
     packages=find_packages(exclude=['*.tests']),
     package_data={'tractags' : [
         'templates/*.html', 'htdocs/js/*.js', 'htdocs/css/*.css',
